66d59570c8d41757219d77048712e042a5850a69
SuyeonPark <89519957+1suyb@users.noreply.github.com>
2025-01-07 11:01:51 +0900

    chore : sound event path 수정

--- RPGProject/Assets/Project/Scripts/Utils/ResourcePath.cs
+++ RPGProject/Assets/Project/Scripts/Utils/ResourcePath.cs
@@ -2,7 +2,7 @@ namespace ResourcePath
 {
     public class SO
     {
-        public const string SoundEvent = "Data/SoundEvent";
+        public const string SoundEvent = "Data/Event/SoundEvent";
         public const string LoadingEvent = "Data/Event/LoadingEvent";
     }
     
